6d8465f5cf623c9a68cba122779312fb540d802a
Nem code <nemcodespace@gmail.com>
2026-05-08 20:35:22 -0300

    atualiza

--- includes/main.php
+++ includes/main.php
@@ -5,9 +5,6 @@
 <main class="main">
   <div class="titulo-top">
   <div class="container">
-    <h2>Site em atualização</h2>
-    <p>Obrigado</p>
-    <p>Obrigado</p>
     <?php
       $quantidade = count($receitas);
       error_reporting(E_ALL);
